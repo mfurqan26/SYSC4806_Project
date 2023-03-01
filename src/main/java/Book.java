37252e7c5809103d84802df3efe5ee5194585f84
Sahil Agrawal <sahilagr21@gmail.com>
2023-03-01 15:38:10 -0500

    first commit just basic stuff

---
+++ src/main/java/Book.java
@@ -0,0 +1,21 @@
+import jakarta.persistence.*;
+
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class Book {
+    @Id
+    private ISBN id;
+    private String name;
+    private String description;
+
+    private String publisher;
+    private ByteArrayInputStream cover;
+
+
+
+
+}

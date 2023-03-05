19f56a9614874f438ae7c82719116c46229bf29e
sabin <sabin.pla@gmail.com>
2023-03-05 02:02:04 -0500

    updated book and renamed inventory

---
+++ src/main/java/BookId.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.lang.Number;
+import java.util.Arrays;
+import java.util.List;
+import java.io.Serializable;
+
+
+public class BookId implements Serializable {
+    private Number[] isbn;
+    private int version;
+}

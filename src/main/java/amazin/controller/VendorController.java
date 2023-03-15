fd2ff5d06db79567a7eae2c9b2032262a9c52b23
Muhammad Furqan <91328394+mfurqan26@users.noreply.github.com>
2023-03-15 19:49:18 -0400

    Create Books for Vendor now working

--- src/main/java/amazin/controller/VendorController.java
+++ src/main/java/amazin/controller/VendorController.java
@@ -26,7 +26,41 @@ public class VendorController {
     }
 
     @GetMapping("/VendorCreate")
-    public String VendorCreate(Model model) {
+    public String VendorCreate() {
+        return "VendorCreate";
+    }
+    @PostMapping(value="/VendorCreate", params = "AddBook")
+    public String CreateBook(@RequestParam(name="isbn", required=false, defaultValue = "") String isbn,
+                               @RequestParam(name="version", required=false, defaultValue = "") String version,
+                               @RequestParam(name="name", required=false, defaultValue = "") String name,
+                               @RequestParam(name="description", required=false, defaultValue = "") String description,
+                               @RequestParam(name="publisher", required=false, defaultValue = "") String publisher,
+                               @RequestParam(name="stock", required=false, defaultValue = "") String stock,
+                               @RequestParam(name="price", required=false, defaultValue = "") String price, Model model) {
+        Boolean conditionAnyNotSet = isbn.equals("") || version.equals("") || name.equals("") ||
+                description.equals("") || publisher.equals("") || stock.equals("") || price.equals("");
+        if(conditionAnyNotSet){
+            model.addAttribute("createBookError", "Some Input is Not Set!");
+        }
+
+        else {
+            try {
+                int newVersion = Integer.parseInt(version);
+                double newPrice = Double.parseDouble(price);
+                int newStock = Integer.parseInt(stock);
+                if(newPrice >= 0 && newStock >= 0 && newVersion >= 1) {
+                    Book newBook = new Book(isbn, newVersion, name, description, publisher, newStock, newPrice);
+                    bookRepository.save(newBook);
+                    return "redirect:/Vendor";
+                }
+                else{
+                    model.addAttribute("createBookError", "Enter Non-Negative numbers for version or stock or price!");
+                }
+            } catch (NumberFormatException nfe) {
+                model.addAttribute("createBookError", "Inputs do not have correct number formating for version or stock or price!");
+            }
+        }
+
         return "VendorCreate";
     }
 

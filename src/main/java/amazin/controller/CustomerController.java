8771a873709b4c07883399ee4331b023f6873a49
nikolaspaterson <nikpaterson@cmail.carleton.ca>
2023-03-21 14:22:25 -0400

    Repurposed Vendor code and creating main shopping page for customers

--- src/main/java/amazin/controller/CustomerController.java
+++ src/main/java/amazin/controller/CustomerController.java
@@ -1,13 +1,33 @@
 package amazin.controller;
 
+import amazin.model.Book;
 import amazin.model.Customer;
+import amazin.repository.BookRepository;
 import amazin.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class CustomerController {
+    @Autowired
     private CustomerRepository customerRepository;
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping("/Shop")
+    public String Customer(Model model){
+        Iterable<Book> books = bookRepository.findAll();
+        model.addAttribute("books", books);
+        return "Shop";
+    }
+
+    @GetMapping("/ShoppingCart")
+    public String Customer(){
+        return "ShoppingCart";
+    }
+
 
     @Autowired
     public CustomerController(CustomerRepository customerRepository){

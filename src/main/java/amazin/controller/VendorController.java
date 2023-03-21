d13ad179c1ec4505fe02cbb748b88a6b5720a8c4
youhengzhou <youhengzhou@cmail.carleton.ca>
2023-03-21 17:36:50 -0400

    init changes

--- src/main/java/amazin/controller/VendorController.java
+++ src/main/java/amazin/controller/VendorController.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-
 @Controller
 public class VendorController {
     @Autowired
